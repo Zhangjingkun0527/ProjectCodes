1d60185c821d9bd4b5e89d071976b9733948e255
zjk740135449 <740135449@qq.com>
2018-10-06 23:08:34 +0800

    little change in weahterForcast

--- 01 JAVA Programming/02 Pratise After Class/JustPractise/src/Test.java
+++ 01 JAVA Programming/02 Pratise After Class/JustPractise/src/Test.java
@@ -2,7 +2,7 @@
 public class Test{
     public static void main(String args[]){
         int nums[] = {0,2,5,7,6,4};
-
+        System.out.println(nums[2]);
     }
 }
 
--- 02 Mobile Development/WeatherForcast/app/src/main/java/com/zjk1800022769/weatherforecast/FirstPage_Activity.java
+++ 02 Mobile Development/WeatherForcast/app/src/main/java/com/zjk1800022769/weatherforecast/FirstPage_Activity.java
@@ -2,11 +2,28 @@ package com.zjk1800022769.weatherforecast;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class FirstPage_Activity extends Activity {
+
     @Override
-    public void onCreate(Bundle forSaveInstance){
+    public void onCreate(Bundle forSaveInstance) {
         super.onCreate(forSaveInstance);
         setContentView(R.layout.firstpage);
+
+        TextView cityWeather1_textView = findViewById(R.id.cityWeather_textView);
+        TextView cityWeather2_textView = findViewById(R.id.city_TextView);
+        cityWeather1_textView.setText(" 北京天气");
+        cityWeather2_textView.setText("北京");
+
+        TextView publishTime_textView = findViewById(R.id.publishTime_TextView);
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+        publishTime_textView.setText("今天" + simpleDateFormat.format(date)+ "发布");
+
     }
 }
